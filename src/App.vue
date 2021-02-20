3cd4864f51af6d645ea700cfdb9ff9a340fe89a9
fafa-a <fablelong@yahoo.fr>
2021-02-20 23:42:50 +0100

    feat: initial commit with TheBanner component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <TheBanner text="Sneakers DB" />
+</template>
+
+<script setup>
+import TheBanner from "./components/TheBanner.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+TheBanner {
+  display: flex;
+}
+</style>
---
+++ src/components/TheBanner.vue
@@ -0,0 +1,28 @@
+<template>
+  <header
+    role="banner"
+    class="container flex items-center justify-around mx-auto"
+  >
+    <img
+      alt="Converse sneakers sketch"
+      src="../assets/img/imgbin_plimsoll-shoe-sneakers-euclidean-png.png"
+    />
+    <h1 class="font-black text-yellow-600 font-display text-9xl">
+      {{ text }}
+    </h1>
+  </header>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  text: String,
+});
+</script>
+
+<style scoped>
+img {
+  width: 35vw;
+}
+</style>
